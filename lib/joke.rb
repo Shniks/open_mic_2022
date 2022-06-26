1a1dff3bec7235453dcafd6fb2ead4cf2cbd07da
Nikhil Shahi <shniks@gmail.com>
2022-06-25 17:08:16 -0700

    created Joke class.

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,2 +1,13 @@
 class Joke
+
+  attr_reader  :id,
+               :setup,
+               :punchline
+
+  def initialize(data)
+    @id = data[:id]
+    @setup = data[:setup]
+    @punchline = data[:punchline]
+  end
+
 end
