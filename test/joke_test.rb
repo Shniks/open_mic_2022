896d2b22dc475104e72c87d23d2bf8c1609e18e4
Nikhil Shahi <shniks@gmail.com>
2022-06-25 17:08:49 -0700

    create Joke Test class and added two tests.

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -1,11 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/joke'
+require '../lib/joke'
 
 class JokeTest < Minitest::Test
-  def test_it_exists
+
+  def test_if_it_exists
+    joke = Joke.new({id: 1, setup: "Why did the strawberry cross the road?", punchline: "Because his mother was in a jam."})
+
+    assert_instance_of Joke, joke
   end
 
   def test_it_has_attributes
+    joke = Joke.new({id: 1, setup: "Why did the strawberry cross the road?", punchline: "Because his mother was in a jam."})
+
+    assert_equal 1, joke.id
+    assert_equal "Why did the strawberry cross the road?", joke.setup
+    assert_equal "Because his mother was in a jam.", joke.punchline
   end
 end
